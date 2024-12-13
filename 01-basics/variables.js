940ee6aff27d2af9bd2f6fc268a257c372de429d
Talha Bashir <contact@talhacodes.com>
2024-12-13 12:40:38 +0000

    variables push

---
+++ 01-basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456;
+let accountEmail = "butttalha806@gmail.com";
+var accountPassword = "123_talha";
+cityName = "Muridke";
+
+// accountId = 4;
+accountEmail = "butttalha805@gmail.com";
+accountPassword = "321_talha";
+cityName = "Lahore";
+console.table([accountId,accountEmail,accountPassword,cityName])
+/*
+  prefer not to use var because of issue in block scope and functional scope
+*/
