7f682ef073755eaeb7513c418cf7207e8a8f6f82
Talha Bashir <contact@talhacodes.com>
2024-12-13 13:57:11 +0000

    datatypes

---
+++ 01-basics/dataTypes.js
@@ -0,0 +1,10 @@
+"use strict"; //treat all code as new version of js 
+//alert("Talha") //we're using node js not browser
+
+let name = "Talh bashir butt"//number
+let number = 123445 //number
+let boolean = true // boolean
+let error = null // null
+let codeTalha;
+
+console.log(typeof codeTalha)
