19432720eb9f9f86d2d136890529c4c8760022e2
Talha Bashir <contact@talhacodes.com>
2024-12-13 14:04:49 +0000

    conversion

---
+++ 01-basics/conversion-operation.js
@@ -0,0 +1,12 @@
+// let numberElement = 23
+
+// let stringNumberelement = String(numberElement)
+// console.log(typeof stringNumberelement)
+
+// let stringElement = "Talha Bashir"
+// let numberStringElement = Number(stringElement)
+// console.log(typeof numberStringElement)
+
+let stringElement = "123"
+let numberStringElement = Boolean(stringElement)
+console.log(numberStringElement)
