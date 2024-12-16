689cbb99511bca06e59853cbd5a4c735c5bb808a
Talha Bashir <contact@talhacodes.com>
2024-12-16 14:01:55 +0000

    string-detail-push

---
+++ 01-basics/strings-in-detail.js
@@ -0,0 +1,25 @@
+
+// Method one for declare string
+// const name = "Talha"
+// const repoCount = 2
+// console.log(`My name is ${name} and my github repo count is ${repoCount}`)
+//Method 2 for decalre string 
+// let newName = new String("My Name is Talha Bashir")
+// // console.log(newName.anchor);
+// let newstringName = "My Name is Talha Bashir"
+// let newArrayName = Array(newstringName)
+// console.log(typeof newArrayName);
+
+
+//***Use of at ***/
+
+// let  arraySuperHero = ["Talha","Nabeel","Usman"]
+// let index = 2
+
+// console.log(`${arraySuperHero.at(index)}`)
+// let  narraySuperHero = ["Talha","Nabeel","Usman"]
+// let nindex = -2
+
+// console.log(`${narraySuperHero.at(nindex)}`)
+
+//***End of at***
