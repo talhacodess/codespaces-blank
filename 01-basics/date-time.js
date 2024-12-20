2da703b8d782dbb614d3b9d3380a244c48cf8e6f
Talha Bashir <contact@talhacodes.com>
2024-12-20 11:21:06 +0000

    array

---
+++ 01-basics/date-time.js
@@ -0,0 +1,46 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(myDate.getHours());
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMilliseconds());
+// console.log(myDate.getMinutes());
+// console.log(myDate.getMonth());
+// console.log(myDate.getSeconds());
+// console.log(myDate.getTime());
+// console.log(myDate.getUTCDate());
+// console.log(myDate.getUTCDay());
+// console.log(myDate.getUTCFullYear());
+// console.log(myDate.getUTCHours());
+// console.log(myDate.getUTCMilliseconds());
+// console.log(myDate.getUTCMinutes());
+// console.log(myDate.getUTCMonth());
+// console.log(myDate.getUTCSeconds());
+// console.log(myDate.getVarDate());
+
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date("12-19-2024")
+
+// // console.log(myCreateDate.toLocaleString())
+
+// let timeStamp =  Date.now()
+
+// console.log(timeStamp)
+// console.log(myCreateDate.getTime())
+// console.log(Date.now()/1000)
+// console.log(Math.floor(Date.now()/1000))
+
+
+
+let newDate = new Date()
+
+console.log(`${newDate.getFullYear()} and month is ${newDate.getMonth() + 1} and Time is ${newDate.toTimeString()}`);
---
+++ 02-basics/arrays.js
@@ -0,0 +1,31 @@
+// let arrayN = [1,2,3,4,5]
+
+// console.log(arrayN.slice(1,4));
+// console.log(arrayN.unshift(10));
+
+const marvalHeros = ["Thor","Ironman","Spiderman"]
+const dcHeros = ["Batman","superman","flash"]
+// marvalHeros.push(dcHeros)
+// console.log(marvalHeros);
+const allHeros  = marvalHeros.concat(dcHeros);
+console.log(allHeros);
+
+// const anotherArray = [1,2,3,[5,6,7],[9,9,0,5,[9,2,3]]]
+// const new_anotherArray = anotherArray.flat(Infinity)
+
+// console.log(new_anotherArray);
+// console.log(new_anotherArray.reverse())
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// const totalScore = Array.of(score1,score2,score3);
+
+// console.log(totalScore);
+
+
+// remove double values from array
+// const arrayDouble = [1,1,4,5,1,7,9,10]
+// let u = new Set(arrayDouble)
+// let a1 = [...u]
+// console.log(a1)
