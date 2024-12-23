71c7435f44cceeb5d373de9fd08573c12a6ed74c
Talha Bashir <contact@talhacodes.com>
2024-12-23 13:16:25 +0000

    functions with object and array

--- 03-basics/functions.js
+++ 03-basics/functions.js
@@ -12,14 +12,42 @@
 // callMyName();
 
 
-function userLoggedIn(username = "Pakistan"){
-    if(!username){
-        console("Please Enter Your Name")
+// function userLoggedIn(username = "Pakistan"){
+//     if(!username){
+//         console("Please Enter Your Name")
 
-    }else{
-        return `${username} is Logged In`
-    }
+//     }else{
+//         return `${username} is Logged In`
+//     }
+
+
+// }
+// console.log(userLoggedIn())
+//rest operation ...num1 
+function calculateCartPrice(val1,val2,...num1){
+
+    return num1
+}
+
+// console.log(
+//  calculateCartPrice(565,23,45,67,98));
+ let user = {
+    name:"Talha",
+    price: 400
+ }
+
+ function objectHandle(anyobjecthere){
+
+    return `The Username is ${anyobjecthere.name} and price of my product is ${anyobjecthere.price}`
+ }
+
+//  console.log(objectHandle(user))
+let myarray = [1,2,3,4,5]
+function returnSecondValue(getArray){
+
+    return getArray[3]
 
 
 }
-console.log(userLoggedIn("usman"))
+// console.log(returnSecondValue(myarray))
+console.log(returnSecondValue([100,200,300,400,500]))
