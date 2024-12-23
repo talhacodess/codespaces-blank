8f500b204048430629a81360656e3348f016f6cb
Talha Bashir <contact@talhacodes.com>
2024-12-23 16:58:43 +0000

    scopes commit

---
+++ 03-basics/scopes.js
@@ -0,0 +1,26 @@
+// function one(){
+//     const username = "Talha bashir"
+
+//     function two(){
+//         const website = "Google"
+//         console.log(username);
+        
+//     }
+
+//     // console.log(website);
+//     two()
+    
+// }
+// one()
+
+if(true){
+    const username = "Usman Butt"
+    if(username === "Usman Butt"){
+        const website = "youtube"
+        console.log(`${username}  ${website}`)
+    }
+    // console.log(website)
+ 
+}
+// console.log(username);
+
