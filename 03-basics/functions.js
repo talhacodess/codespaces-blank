d4bc5b3756686c08212437cc72d6cd0b7a6c05b7
Talha Bashir <contact@talhacodes.com>
2024-12-23 11:52:10 +0000

    function logic learn

---
+++ 03-basics/functions.js
@@ -0,0 +1,25 @@
+
+
+// function callMyName(){
+
+//     console.log("T")
+//     console.log("a")
+//     console.log("l")
+//     console.log("h")
+//     console.log("a")
+// }
+
+// callMyName();
+
+
+function userLoggedIn(username = "Pakistan"){
+    if(!username){
+        console("Please Enter Your Name")
+
+    }else{
+        return `${username} is Logged In`
+    }
+
+
+}
+console.log(userLoggedIn("usman"))
