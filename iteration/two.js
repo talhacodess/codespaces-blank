037b42e7ea170fb72f7098db58edbd0654cde6df
Talha Bashir <contact@talhacodes.com>
2024-12-26 18:45:55 +0000

    do-while

---
+++ iteration/two.js
@@ -0,0 +1,17 @@
+// let a = 0
+// while (a <=10) {
+//     console.log(`the value of a is : ${a}`)
+//     a = a +2
+// }
+
+// let arr = ["Moon","sun","Earth"]
+// let b = 0
+// while(b< arr.length){
+//     console.log(`array is ${arr[b]}`)
+//     b++
+// }
+let score = 20
+do {
+    console.log(`the score is : ${score}`)
+    score ++
+} while (score< 10)
