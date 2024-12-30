06a4bcfce34a9c72633dee026fbbff8057549c78
Talha Bashir <contact@talhacodes.com>
2024-12-30 17:44:57 +0000

    commit

--- iteration/three.js
+++ iteration/three.js
@@ -9,9 +9,31 @@ for (const j of arr) {
 const greeting = "Hello World"
 for (const greet of greeting ) {
    
-    console.log(`${greet} and ${greeting}`);
+    // console.log(`${greet} and ${greeting}`);
     
 }
 
 //maps
 const map = new Map()
+map.set("a","1")
+map.set("b","2")
+map.set("c","3")
+map.set("d","4")
+map.set("d","4")
+// console.log(map)
+for (const [key,value] of map) {
+    // console.log(`${key} :- ${value}`);
+    
+}
+//object 
+const myObject = {
+    name: "Talha",
+    age: 20
+
+}
+
+for (const f in myObject) {
+    console.log(myObject);
+    
+    
+}
