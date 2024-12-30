1266e6de1b6ab4bf029fdc5f907daae19a1fd6c8
Talha Bashir <contact@talhacodes.com>
2024-12-30 15:01:23 +0000

    commit

---
+++ iteration/three.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4,5,6]
+
+for (const j of arr) {
+    // console.log(`${j} is variable`);
+    
+    
+}
+
+const greeting = "Hello World"
+for (const greet of greeting ) {
+   
+    console.log(`${greet} and ${greeting}`);
+    
+}
+
+//maps
+const map = new Map()
