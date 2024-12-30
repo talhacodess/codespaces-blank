2d6e9a9590c6df6bd113996297a69921d283f7ab
Talha Bashir <contact@talhacodes.com>
2024-12-30 18:27:48 +0000

    commit

---
+++ iteration/four.js
@@ -0,0 +1,32 @@
+// const coding  = ["ruby","js","phython","java","cpp"]
+// coding.forEach( (data)=>{
+//     console.log(data);
+    
+// })
+const coding  = ["ruby","js","phython","java","cpp"]
+coding.forEach( (data,index,arr)=>{
+    // console.log(data);
+    
+})
+
+myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName:"js"
+    },
+    {
+        languageName: "java",
+        languageFileName:"java"
+    },
+    {
+        languageName: "phython",
+        languageFileName:"py"
+    }
+
+]
+myCoding.forEach((item)=>{
+
+    console.log(item.languageName);
+    
+
+})
