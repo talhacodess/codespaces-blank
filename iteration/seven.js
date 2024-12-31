e8abdb367550722baeda1d45496794ae18d515ea
Talha Bashir <contact@talhacodes.com>
2024-12-31 11:56:08 +0000

    commit2

--- iteration/seven.js
+++ iteration/seven.js
@@ -1,1 +1,6 @@
-it
+let arr = [1,2,3,4,5,6,7,8,9,10]
+let newArr = arr
+                .map((num)=> num +10)
+                .map((num)=> num *2)
+                .filter((num)=> num >=24)
+console.log(newArr);
